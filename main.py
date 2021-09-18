ed5660336b8b767ce4d64d5a2ca7dfea220a8c11
Neel Adwani <nprosofficial@gmail.com>
2021-09-18 07:11:35 +0000

    added backend structure

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, make_response, redirect, url_for
+
+
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+
+# Index page and Rendering Basic Templates
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+if __name__ == '__main__':
+  # Run the Flask app
+  app.run(
+	host='0.0.0.0',
+	debug=True,
+	port=8080
+  )
